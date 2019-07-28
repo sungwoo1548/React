c27a88b8c40527244d4d1eee02d320e07b781e43
sungwoo1548 <swo1548@gmail.com>
2019-07-29 01:57:57 +0900

    ch14_예제_get요청 ip주소 얻기

--- ch14_외부데이터사용/ipaddress/src/IPAddressContainer.js
+++ ch14_외부데이터사용/ipaddress/src/IPAddressContainer.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 class IPAdressContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ip_address: "...",
+        };
+
+        this.processRequest = this.processRequest.bind(this);
+    }
+
+    processRequest(){
+        $.get('http://ip.jsontest.com/',(data,status)=>{
+            this.setState({
+                ip_address : data.ip,
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.processRequest();
+    }
+    
     render() {
         return (
-            <p>Nothing yet!</p>
+            <p>{this.state.ip_address}</p>
         );
     }
 }
