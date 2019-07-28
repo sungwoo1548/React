0f8c0dfbf37a9513aa17e25c89c1361dea9d5ab8
sungwoo1548 <swo1548@gmail.com>
2019-07-28 23:05:25 +0900

    ch13_reactAPP_helloworld

---
+++ ch13_Development_environment/helloworld/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class HelloWorld extends Component{
+    render(){
+        return(
+            <div className="helloContainer">
+                <h1>Hello, World~!!!</h1>
+            </div>
+        );
+    }
+
+}
+
+export default HelloWorld;
