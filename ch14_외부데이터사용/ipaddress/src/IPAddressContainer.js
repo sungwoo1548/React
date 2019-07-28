46dd6d852cc59eb47b4f8acef188faa4ffd484b0
sungwoo1548 <swo1548@gmail.com>
2019-07-29 01:37:16 +0900

    ch14_예제 셋팅

---
+++ ch14_외부데이터사용/ipaddress/src/IPAddressContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class IPAdressContainer extends Component {
+    render() {
+        return (
+            <p>Nothing yet!</p>
+        );
+    }
+}
+
+export default IPAdressContainer;
