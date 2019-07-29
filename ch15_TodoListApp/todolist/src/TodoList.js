512657e6c6b4dc69c8c2ddc5cfecc254b62190b0
sungwoo1548 <swo1548@gmail.com>
2019-07-29 22:33:57 +0900

    ch15_TodoListApp_TodoList기능추가/setstate,ref,event

--- ch15_TodoListApp/todolist/src/TodoList.js
+++ ch15_TodoListApp/todolist/src/TodoList.js
@@ -1,12 +1,41 @@
 import React, {Component} from "react";
 
 class TodoList extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            items : [],
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem(e){
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== ""){
+            itemArray.unshift({
+                text : this._inputElement.value,
+                key : Date.now(),
+            });
+
+            this.setState({
+                items : itemArray,
+            });
+
+            // this._inputElement.focus();
+            this._inputElement.value="";
+        } 
+        console.log(itemArray);
+
+        e.preventDefault();
+    }
     render(){
         return(
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="enter task"></input>
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a)=>this._inputElement = a} placeholder="enter task"></input>
                         <button type="submit">add</button>
                     </form>
                 </div>
