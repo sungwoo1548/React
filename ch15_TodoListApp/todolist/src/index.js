0175b79c3b9af825ef7e8c9e0b87e1cf64b79c9a
sungwoo1548 <swo1548@gmail.com>
2019-07-29 22:09:19 +0900

    ch15_TodoListApp_예제 셋팅

---
+++ ch15_TodoListApp/todolist/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+var destination = document.querySelector('#container');
+
+ReactDOM.render(
+    <div>
+        test
+    </div>
+    ,destination
+);
