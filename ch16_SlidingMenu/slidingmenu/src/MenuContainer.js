a9a168f4cde417bb340a6a7519e7f0306108764e
sungwoo1548 <swo1548@gmail.com>
2019-07-30 11:40:18 +0900

    ch16_SlidingMenu_예제 셋팅

---
+++ ch16_SlidingMenu/slidingmenu/src/MenuContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class MenuContianer extends Component{
+    render(){
+        return(
+            <div>
+                menucontainer test
+            </div>
+        );
+    }
+
+}
+
+export default MenuContianer;
