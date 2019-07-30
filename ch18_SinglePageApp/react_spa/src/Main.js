18fdd1c52af2e79450b7c2857e4ad9ac33bb7545
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:05:07 +0900

    ch18_SPA_메인컴포넌트 설정

--- ch18_SinglePageApp/react_spa/src/Main.js
+++ ch18_SinglePageApp/react_spa/src/Main.js
@@ -4,7 +4,12 @@ class Main extends Component{
     render(){
         return(
             <div>
-                test
+                <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/stuff">Stuff</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
             </div>
         );
     }
