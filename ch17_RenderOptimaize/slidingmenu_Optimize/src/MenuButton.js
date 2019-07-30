b6616b3c783cb2b865bb09de0d914a4fbb68492e
sungwoo1548 <swo1548@gmail.com>
2019-07-30 21:33:44 +0900

    ch17_최적화_MenuButton_첫번째

--- ch17_RenderOptimaize/slidingmenu_Optimize/src/MenuButton.js
+++ ch17_RenderOptimaize/slidingmenu_Optimize/src/MenuButton.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import "./MenuButton.css";
 
 class MenuButton extends Component {
+    shouldComponentUpdate(nextPorps, nextState){
+        if (nextPorps.handleMouseDown === this.props.handleMouseDown){
+            return false;
+        } else {
+            return true;
+        }
+    }
     render() {
         console.log("Rendering : MenuButton");
         return (
