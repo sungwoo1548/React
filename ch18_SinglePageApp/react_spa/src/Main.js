40e9e8c2b7b089285dc440663a85e6ffb2a8dbe7
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:23:42 +0900

    ch18_SPA_라우팅_react-router-dom모듈 사용

--- ch18_SinglePageApp/react_spa/src/Main.js
+++ ch18_SinglePageApp/react_spa/src/Main.js
@@ -1,16 +1,31 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
 
-class Main extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Simple SPA</h1>
-                <ul className="header">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/stuff">Stuff</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
+class Main extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Simple SPA</h1>
+                    <ul className="header">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/stuff">Stuff</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route path="/" component={Home}></Route>
+                        <Route path="/stuff" component={Stuff}></Route>
+                        <Route path="/contact" component={Contact}></Route>
+                    </div>
+                </div>
+            </HashRouter>
         );
     }
 }
