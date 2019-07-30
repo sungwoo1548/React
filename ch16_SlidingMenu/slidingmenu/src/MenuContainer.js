3192164a55e51fd60e3d7387a4723b72d37425ed
sungwoo1548 <swo1548@gmail.com>
2019-07-30 11:54:02 +0900

    ch16_SlidingMenu_이벤트 함수 설정

--- ch16_SlidingMenu/slidingmenu/src/MenuContainer.js
+++ ch16_SlidingMenu/slidingmenu/src/MenuContainer.js
@@ -1,13 +1,50 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class MenuContianer extends Component{
-    render(){
-        return(
+class MenuContianer extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            visible : false,
+        };
+
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    };
+
+    handleMouseDown(e){
+        this.toggleMenu();
+
+        console.log("Mouse Clicked!");
+        e.stopPropagation();
+    };
+
+    toggleMenu(){
+        this.setState({
+            visible : !this.state.visible,
+        });
+    };
+
+    render() {
+        return (
             <div>
-                menucontainer test
+                <div>
+                    <p>Can you spot the item that doesn't belong?</p>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Ipsum</li>
+                        <li>Dolor</li>
+                        <li>Sit</li>
+                        <li>Bumblebees</li>
+                        <li>Aenean</li>
+                        <li>Consectetur</li>
+                    </ul>
+                </div>
+                <button onClick={this.handleMouseDown}>test</button>
             </div>
         );
-    }
+    };
 
 }
 
