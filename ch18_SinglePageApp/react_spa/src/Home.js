f9385adc15912a7ef8f5e4ddef5acc090592e799
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:12:50 +0900

    ch18_SPA_뷰페이지 작성 3개

---
+++ ch18_SinglePageApp/react_spa/src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HELLO</h2>
+                <p>첫번째 단락 내용입니다. 00000000000000
+                    120031002190492104901294091204
+                    2190390129049102940129
+                    401920930219ㄷㄴ머ㅣ러ㅣㄴ머린머ㅣ러민ㄹ
+                    ㅐㅕ3ㅐㅑㅓㄹ인머ㅣㄹ언미러임
+                </p>
+                
+                <p>두번째 단락 내용입니다.----------------------</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ ch18_SinglePageApp/react_spa/src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>STUFF</h2>
+                <p>stuff의 첫번째 단락 내용입니다.</p>
+                <ol>
+                    <li>Nulla pulvinar diam</li>
+                    <li>Nulla pulvinar diam</li>
+                    <li>Nulla pulvinar diam</li>
+                    <li>Nulla pulvinar diam</li>
+                    <li>Nulla pulvinar diam</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
