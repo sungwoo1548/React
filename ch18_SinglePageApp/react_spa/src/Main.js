e48329a75d9a010fb2dfb1362d44102a1f3864e3
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:34:00 +0900

    ch18_SPA_스타일적용2

--- ch18_SinglePageApp/react_spa/src/Main.js
+++ ch18_SinglePageApp/react_spa/src/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
                 <div>
                     <h1>Simple SPA</h1>
                     <ul className="header">
-                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
                     </ul>
