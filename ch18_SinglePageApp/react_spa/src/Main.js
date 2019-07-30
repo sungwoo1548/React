e4a38dd778943b9f3581904dd048423e9599e16b
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:01:21 +0900

    ch18_SinglePageApp_예제 셋팅

---
+++ ch18_SinglePageApp/react_spa/src/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default Main;
