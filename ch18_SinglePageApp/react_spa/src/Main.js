38fbe2502ffde41f4d41ecba1d84bbda5fdb7882
sungwoo1548 <swo1548@gmail.com>
2019-07-30 22:31:47 +0900

    ch18_SPA_스타일적용

--- ch18_SinglePageApp/react_spa/src/Main.js
+++ ch18_SinglePageApp/react_spa/src/Main.js
@@ -20,7 +20,7 @@ class Main extends Component {
                         <li><NavLink to="/contact">Contact</NavLink></li>
                     </ul>
                     <div className="content">
-                        <Route path="/" component={Home}></Route>
+                        <Route exact path="/" component={Home}></Route>
                         <Route path="/stuff" component={Stuff}></Route>
                         <Route path="/contact" component={Contact}></Route>
                     </div>
